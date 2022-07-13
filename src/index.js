09ee4532da39345adbebe582975131b414908bb2
Isac Tomaz da Silva <isactomaz@protonmail.com>
2022-07-13 15:32:34 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 require('./controllers/auth.controller')(app);
 require('./controllers/task.controller')(app);
